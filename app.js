82bf63e206655be3cea773b1f1715a4e2373180a
unknown <yusuf.chnd@gmail.com>
2022-08-23 23:04:48 +0700

    lab info

--- app.js
+++ app.js
@@ -1,8 +1,17 @@
-const bcghov = document.getElementById('logo');
-
-bcghov.addEventListener("mouseenter", (event) => {
-    document.getElementById('background').style.filter="brightness(200%)";
-})
-bcghov.addEventListener("mouseleave", (event) => {
-    document.getElementById('background').style.filter="brightness(100%)";
-})
+const links = document.querySelectorAll( ".logo" );
+for ( let i = 0; i < links.length; i++ ) {
+  const link = links[ i ];
+  link.addEventListener( "mouseover", function() {
+    document.documentElement.style.setProperty('--bright', "brightness(100%)")
+  });
+  link.addEventListener( "mouseout", function() {
+    document.documentElement.style.setProperty('--bright', "brightness(20%)")
+  });
+}
+const logohov = document.getElementById('logohov');
+logohov.addEventListener("mouseover", function(){
+  document.documentElement.style.setProperty('--white', "green")
+});
+logohov.addEventListener("mouseout", function(){
+  document.documentElement.style.setProperty('--white', "white")
+});
